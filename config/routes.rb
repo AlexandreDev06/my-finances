0cf1ef433b8083dc31793853a14c3fe772057ad8
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-09-19 18:44:23 -0300

    Resolvendo conflitos

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 Rails.application.routes.draw do
   root "home#index"
   resources :expenses
 end
-=======
-Rails.application.routes.draw do
-  root "home#index"
-end
->>>>>>> 61a082555e3572fcfd7e2a7db02ba634dcea89d2
