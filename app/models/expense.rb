048cf259fd1c00a2bcf48e0e65eebe81c7e0bc9e
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-09-30 20:52:04 -0300

    poupanca adicionada

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,4 @@
 class Expense < ApplicationRecord
+  enum flux: [:balance, :saving]
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_234440) do
+ActiveRecord::Schema.define(version: 2021_09_30_232210) do
 
   create_table "credits", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_234440) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "flux"
     t.index ["user_id"], name: "index_credits_on_user_id"
   end
 
@@ -31,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_234440) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "flux"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
