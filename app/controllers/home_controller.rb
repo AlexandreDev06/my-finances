0cab47d166b7d2c62257e34e99b6f0e3e31655f7
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-10-03 03:20:33 -0300

    crud de futuros objetivos feito

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,12 @@ class HomeController < ApplicationController
     @expenses = Expense.all
     @user = User.first
     @credits = Credit.all
+    @future_goals = FutureGoal.all
     @total_expense = @expenses.sum { |e| e.total }
     @total_credit = @credits.sum { |c| c.total }
     @users = User.order :created_at
     @total_balance = @credits.balance.sum { |c| c.total } - @expenses.balance.sum { |e| e.total }
     @total_savings = @credits.saving.sum { |c| c.total } - @expenses.saving.sum { |e| e.total }
+    @total_future = @future_goals.sum { |f| f.total }
   end
 end
