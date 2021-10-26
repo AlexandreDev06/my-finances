735e2c13573c37ac9bbd3a2f2283e7d2ec69a708
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-10-25 21:14:16 -0300

    devise adicionado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_030646) do
+ActiveRecord::Schema.define(version: 2021_10_25_233503) do
 
   create_table "credits", force: :cascade do |t|
     t.string "name"
@@ -67,11 +67,18 @@ ActiveRecord::Schema.define(version: 2021_10_23_030646) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "name"
     t.float "balance"
     t.float "savings"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "credits", "users"
