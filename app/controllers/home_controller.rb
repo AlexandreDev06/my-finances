aa83bfd4c0b78fd055147911b596df59b93a8da2
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-10-27 00:44:07 -0300

    aplicando melhorias

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @expenses = Expense.all
     @credits = Credit.all
-    @total_balance = @credits.sum { |c| c.payment_at <= Date.current ? c.total : 0 } - @expenses.sum { |e| e.payment_at <= Date.current ? e.total : 0 }
+    @total_balance = @credits.balance.sum { |c| c.payment_at <= Date.current ? c.total : 0 } - @expenses.balance.sum { |e| e.payment_at <= Date.current ? e.total : 0 }
   end
 end
