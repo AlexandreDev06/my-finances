5e1f615fef35e9b73c5b43e603a36de99466e91a
gamai <gabriel.maires12@gmail.com>
2021-10-26 23:23:05 -0300

    aplicando melhorias

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_233503) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "flux"
+    t.integer "flux", default: 0
     t.index ["user_id"], name: "index_credits_on_user_id"
   end
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_233503) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "flux"
+    t.integer "flux", default: 0
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
