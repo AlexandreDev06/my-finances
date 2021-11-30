4ea682a574d7f2f9f30a2bca777477ddfc48edde
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-11-29 23:45:47 -0300

    ordernacao de valores alterados

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @expenses = Expense.all
-    @credits = Credit.all
+    @expenses = Expense.all.order(payment_at: :desc)
+    @credits = Credit.all.order(payment_at: :desc)
+    @user = current_user
     @total_balance = @credits.balance.sum { |c| c.payment_at <= Date.current ? c.total : 0 } - @expenses.balance.sum { |e| e.payment_at <= Date.current ? e.total : 0 }
   end
 end
