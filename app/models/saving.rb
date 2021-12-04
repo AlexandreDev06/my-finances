efa288a5fb08c9782974a73fb9ecb8dbaaf8f017
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-12-03 21:46:32 -0300

    alteracao de metodos

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -2,15 +2,15 @@ class Saving < ApplicationRecord
   has_many :savings
   belongs_to :user
 
-  def profit_annually(saving)
-    (Credit.total_credit_savings / 100) * saving.profit
+  def profit_annually(saving, current_user)
+    (current_user.credits.total_credit_savings / 100) * saving.profit
   end
 
-  def profit_monthly(saving)
-    saving.profit_annually(saving) / 12
+  def profit_monthly(saving, current_user)
+    saving.profit_annually(saving, current_user) / 12
   end
 
-  def profit_daily(saving)
-    saving.profit_monthly(saving) / 30
+  def profit_daily(saving, current_user)
+    saving.profit_monthly(saving, current_user) / 30
   end
 end
