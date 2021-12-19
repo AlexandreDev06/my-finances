31b43f57a413ceab804b16eecfeb519c4f924aa2
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-12-19 00:24:50 -0300

    metodo corrigido

--- app/models/future_goal.rb
+++ app/models/future_goal.rb
@@ -2,9 +2,9 @@ class FutureGoal < ApplicationRecord
   enum priority: [:no_need, :need, :urgent]
   belongs_to :user
 
-  def self.total_goals
+  def self.total_goals current_user
     totaln = 0
-    FutureGoal.all.sum { |f| totaln += f.total }
+    current_user.future_goals.all.sum { |f| totaln += f.total }
     totaln
   end
 end
